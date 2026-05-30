833792b734a5f56fdbc3354e9365cefaa499384c
Pintu Prasad <pintu8271264091@gmail.com>
2026-05-30 23:59:03 +0530

    Add files via upload

---
+++ password-toggle.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export function App() {
+
+    const [show, setShow] = useState(false);
+
+    return (
+        <div className="container p-5">
+
+            <h2>Password Toggle</h2>
+
+            <input
+                type={show ? "text" : "password"}
+                placeholder="Enter Password"
+                className="form-control w-25"
+            />
+
+            <button
+                onClick={() => setShow(!show)}
+                className="btn btn-primary mt-3"
+            >
+                {show ? "Hide" : "Show"}
+            </button>
+
+        </div>
+    )
+}
