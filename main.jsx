7aae0c59becdd232736b70c543af5078b11ba07e
Pintu Prasad <pintu8271264091@gmail.com>
2026-05-31 00:14:51 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { App } from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
-);
+import { PasswordToggle } from './components/password-toggle-demo/password-toggle'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <PasswordToggle />
+  </StrictMode>,
+)
